6dd17ae6cf6478a98e9b993d2fc7a27524dbba63
wagnerlim <wagnerlima0910@gmail.com>
2021-02-26 11:00:57 -0300

    Pequenas alterações nos cookies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,19 +13,19 @@ import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 
 interface HomeProps {
-  level,
-  currentExperience,
-  challengesCompleted,
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
 }
 
-export default function Home(props) {
+export default function Home(props: HomeProps) {
 
   return (
     <ChallengesProvider
-     level = {props.level} 
-     currentExperience={props.currentExperience}
-     challengesCompleted={props.challengesCompleted}
-     >
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
       <div className={styles.container}>
         <Head>
           <title>Inicio | move.it </title>
@@ -49,13 +49,13 @@ export default function Home(props) {
   )
 }
 
-export const getServerSideProps:GetServerSideProps = async (ctx)=> {
-   const {level, currentExperience, challengesCompleted} = ctx.req.cookies;
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { level, currentExperience, challengesCompleted } = ctx.req.cookies;
 
-  
+
 
   return {
-    props:{
+    props: {
       level: Number(level),
       currentExperience: Number(currentExperience),
       challengesCompleted: Number(challengesCompleted),
